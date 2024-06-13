c7d6b1c75a3e24386d5010e9294fd336f1d8b82f
Eduardo Martins <144805284+duMartinss@users.noreply.github.com>
2024-06-13 19:14:26 -0300

    Update cpf.rb

--- cpf.rb
+++ cpf.rb
@@ -1,3 +1,8 @@
+#Validar CPF.
+#Ponha 'gem install cpf_cnpj' no terminal e instale a gem.
+#Não se esqueça de digitar o require no começo do seu código.
+#Gem do desenvolvedor Nando Vieira, (https://rubygems.org/gems/cpf_cnpj).
+
 require 'cpf_cnpj'
 
 print "Digite o seu cpf: "
